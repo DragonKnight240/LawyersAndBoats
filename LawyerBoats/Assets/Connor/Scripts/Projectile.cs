b69d5b0c35db4210d0b0ff8f8a368461eb62a2c2
Carabu <connorregan25@gmail.com>
2022-10-11 02:06:08 +0100

    arrow tower
    1

---
+++ LawyerBoats/Assets/Connor/Scripts/Projectile.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private Transform target;
+    public float speed = 0.2f;
+    public void Track (Transform targetPos)
+    {
+        target = targetPos;
+    }
+
+    
+    void Update()
+    {
+        if (target == null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        Vector3 direction = target.position - transform.position;
+        float distanceUpdate = speed * Time.deltaTime;
+
+        if (direction.magnitude <= distanceUpdate)
+        {
+            //enemy hit
+            Hit();
+            return;
+        }
+
+        transform.Translate(direction.normalized * distanceUpdate, Space.World);
+
+    }
+
+    void Hit()
+    {
+        Debug.Log("projhit");
+        Destroy(this.gameObject);
+    }
+}
