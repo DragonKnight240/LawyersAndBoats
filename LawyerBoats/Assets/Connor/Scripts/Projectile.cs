cf7cba73eec512ad6aca77460dc59ab2bd3320af
Carabu <connorregan25@gmail.com>
2022-10-18 00:30:14 +0100

    bomb
    1

--- LawyerBoats/Assets/Connor/Scripts/Projectile.cs
+++ LawyerBoats/Assets/Connor/Scripts/Projectile.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     private Transform target;
+
     public float speed = 2.0f;
-    public void Track (Transform targetPos)
+    public float explosionRadius = 2.0f;
+    public bool explosive = false;
+
+    public void Track(Transform targetPos)
     {
         target = targetPos;
     }
 
-    
+
     void Update()
     {
         if (target == null)
@@ -36,7 +40,39 @@ public class Projectile : MonoBehaviour
 
     void Hit()
     {
-        Debug.Log("projhit");
+        if (explosive)
+        {
+            Debug.Log("Exploded");
+            Explode();
+        }
+        else
+        {
+            Debug.Log("Fail");
+            Damage();
+        }
+    }
+
+    void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (Collider collider in colliders)
+        {
+            if (collider.tag == "Enemy")
+            {
+                Destroy(collider.gameObject);
+            }
+        }
+    }
+
+    void Damage()
+    {
+        Destroy(target.gameObject);
         Destroy(this.gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
