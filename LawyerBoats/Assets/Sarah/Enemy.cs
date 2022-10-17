d38377be690c9db0162760ff3c35057296e9c796
Sarah Butlin <91489047+DragonKnight240@users.noreply.github.com>
2022-10-17 20:10:14 +0100

    Enemy Damage & Money

--- LawyerBoats/Assets/Sarah/Enemy.cs
+++ LawyerBoats/Assets/Sarah/Enemy.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
     public float Speed;
     Rigidbody RB;
     BoxCollider Collider;
     internal Patrol PatrolComp;
+    public int Damage = 2;
+    public int Money;
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +22,15 @@ public class EnemyMovement : MonoBehaviour
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, PatrolComp.PatrolTo.transform.position, Time.deltaTime * Speed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Base"))
+        {
+            GameManager.Instance.loseHealth(Damage);
+            GameManager.Instance.addMoney();
+            Destroy(this.transform.parent.gameObject);
+        }
     }
 }
